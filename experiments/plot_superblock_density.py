a5eba2ec1acadf0e451b21648bcabccdd9e5b310
Kostis Karantias <kkarantias@gmail.com>
2019-04-05 19:18:11 +0300

    update superblock density plot to show expectation

---
+++ experiments/plot_superblock_density.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker
+
+from science import *
+
+from collections import Counter
+
+def level_var(hdr):
+    blkid = bitcoin_hdr_to_id(hdr)
+    target = header_to_target(hdr)
+    return level(blkid, target=target)
+
+MAX_LEVEL = 6
+levels_df = pd.DataFrame(bitcoin_core_headers()).applymap(level_var)[0]
+
+WINDOW_SIZE = 1000
+
+def window_size(level):
+    return WINDOW_SIZE * 2**level + 1
+
+def levels():
+    yield from range(MAX_LEVEL+1)
+
+def windowed_counts(df):
+    counts = [0 for lvl in range(MAX_LEVEL+1)]
+    new_els = [window_size(lvl) for lvl in range(MAX_LEVEL+1)]
+    length = len(df)
+
+    max_window_size = window_size(MAX_LEVEL)
+    center_el = max_window_size // 2
+
+    assert center_el + max_window_size // 2 + 1 < length
+
+    for i in range(min(length, max_window_size)):
+        lvl = df[i]
+        for l in range(lvl+1):
+            if l <= MAX_LEVEL:
+                left_el = center_el - window_size(l) // 2
+                right_el = center_el + window_size(l) // 2
+                if left_el <= i <= right_el:
+                    counts[l] += 1
+
+    yield {k: counts[k]/window_size(k) for k in levels()}
+    while center_el + max_window_size // 2 + 1 < length:
+        for lvl in levels():
+            old_el = center_el - window_size(lvl) // 2
+            new_el = center_el + window_size(lvl) // 2 + 1
+            if df[old_el] >= lvl:
+                counts[lvl] -= 1
+            if df[new_el] >= lvl:
+                counts[lvl] += 1
+        yield {k: counts[k]/window_size(k) for k in levels()}
+        center_el += 1
+
+def binomial_expectation(n, p):
+    return n*p
+
+def binomial_std(n, p):
+    return math.sqrt(n*p*(1-p))
+
+def bounds_for_level(lvl):
+    expectation = binomial_expectation(window_size(lvl), 2**(-lvl)) / window_size(lvl)
+    std = binomial_std(window_size(lvl), 2**(-lvl)) / window_size(lvl)
+    return (expectation - std, expectation + std)
+
+def draw_expectations(ax):
+    for lvl in levels():
+        bottom, top = bounds_for_level(lvl)
+        ax.axhspan(bottom, top, facecolor=ax.get_lines()[lvl].get_color(), alpha=0.35)
+
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages('../figures/bitcoin-superblock-distribution.pdf')
+win = pd.DataFrame(windowed_counts(levels_df), dtype='int32').fillna(0)
+win.columns = win.columns.to_series().apply(lambda mu: '%d-superblocks' % mu)
+ax = win.plot(logy=True)
+draw_expectations(ax)
+ax.set(ylabel="superblock density", xlabel="block height")
+ax.set_yscale('log', basey=2)
+plt.savefig(pp, format='pdf', dpi=1000, bbox_inches='tight')
+pp.close()
---
+++ experiments/science.py
@@ -0,0 +1,106 @@
+from hashlib import sha256, scrypt
+import math
+import itertools
+import struct
+
+def doublesha256(x):
+    return sha256(sha256(x).digest()).digest()
+
+def bitcoin_hdr_to_id(hdr):
+    return doublesha256(hdr)
+
+def litecoin_hdr_to_id(hdr):
+    return scrypt(password=hdr, salt=hdr, n=1024, r=1, p=1, dklen=32)
+
+BITCOIN_TARGET =  0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+LITECOIN_TARGET = 0x00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+
+def hash_to_num(h):
+    if isinstance(h, bytes):
+        return int.from_bytes(h, 'little')
+    elif isinstance(h, str):
+        return int(block_id, 16)
+    else:
+        return h
+
+def hdrs_from_file(file_name):
+    with open(file_name, "rb") as f:
+        for height in itertools.count():
+            hdr = f.read(HEADER_SIZE)
+            if len(hdr) != HEADER_SIZE:
+                break
+            yield hdr
+
+BLOCK_ID_SIZE = 32
+
+def blkids_from_blkid_file(file_name):
+    with open(file_name, "rb") as f:
+        while True:
+            blkid = f.read(BLOCK_ID_SIZE)
+            if len(blkid) != BLOCK_ID_SIZE:
+                return
+            yield blkid
+
+def blkids_from_hdr_file(file_name, hdr_to_id=bitcoin_hdr_to_id):
+    for hdr in hdrs_from_file(file_name):
+        yield hdr_to_id(hdr)
+
+def level(block_id, target=None):
+    target = BITCOIN_TARGET if target is None else target
+    block_id = hash_to_num(block_id)
+    return -int(math.ceil(math.log(float(block_id) / target, 2)))
+
+HEADER_SIZE = 80
+
+def levels_from_blkids(blkids, target=None):
+    for blkid in blkids:
+        yield level(blkid, target)
+
+def bitcoin_cash_blkids():
+    yield from blkids_from_hdr_file("BitcoinCash-Mainnet.bin")
+def bitcoin_core_blkids():
+    yield from blkids_from_hdr_file("BitcoinCore-Mainnet.bin")
+def litecoin_blkids():
+    yield from blkids_from_blkid_file("Litecoin-Mainnet-ids.bin")
+
+def bitcoin_cash_levels():
+    yield from levels_from_blkids(bitcoin_cash_blkids())
+def bitcoin_core_levels():
+    yield from levels_from_blkids(bitcoin_core_blkids())
+def litecoin_levels():
+    yield from levels_from_blkids(litecoin_blkids(), target=LITECOIN_TARGET)
+
+def bitcoin_core_headers():
+    yield from hdrs_from_file("BitcoinCore-Mainnet.bin")
+
+def bits_to_target(bits):
+    # bits to target
+    bitsN = (bits >> 24) & 0xff
+    # print('bitsN: %s' % bitsN)
+    assert bitsN >= 0x03 and bitsN <= 0x1d, "First part of bits should be in [0x03, 0x1d]"
+    bitsBase = bits & 0xffffff
+    # print('bitsBase: %s' % hex(bitsBase))
+    assert bitsBase >= 0x8000 and bitsBase <= 0x7fffff, "Second part of bits should be in [0x8000, 0x7fffff]"
+    target = bitsBase << (8 * (bitsN-3))
+    return target
+
+def header_to_bits(hdr):
+    return struct.unpack("<L", hdr[72:76])[0]
+
+def header_to_target(hdr):
+    return bits_to_target(header_to_bits(hdr))
+
+from matplotlib import rc
+import matplotlib.pyplot as plt
+plt.rcParams['text.latex.preamble'] = [r'''
+    \usepackage{lmodern}
+    \usepackage{amsfonts}
+''']
+rc('text', usetex=True)
+rc(
+    'font',
+    family='serif',
+    serif=['Computer Modern Roman'],
+    monospace=['Computer Modern Typewriter'],
+    size=11
+)
